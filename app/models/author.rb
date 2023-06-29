3c8078a5358edcc384e0fc20b02867fb33fdd5eb
Martin <Martin@Martin>
2023-06-29 04:16:58 +0300

    Completed assignment

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+     validates :name, uniqueness: { case_sensitive: true }, presence: true
+    validates :phone_number, length: { is: 10 }, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :summary, length: { maximum: 250 }, presence: true
+    validates :content, length: { minimum: 250 }, presence: true
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :title_not_true_facts
+
+    def title_not_true_facts
+      if title == "True Facts"
+        errors.add(:title, "cannot be set to 'True Facts'")
+      end
+    end
+
 end
